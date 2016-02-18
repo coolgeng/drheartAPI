636bc32cee42f1b059a775a48cdf6e370c4a7d99
Yong Geng <ygeng@L-SB8J53JG8W-M.local>
2016-02-18 15:53:09 -0800

    add 404 and 500 error handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 # include ActionController::Serialization
 
 class ApplicationController < ActionController::API
-    rescue_from ActiveRecord::RecordNotFound, with: :four_oh_four
-
-    def four_oh_four
-      render file: Rails.root.join("public", "error.json"), status: 404
+    rescue_from ActiveRecord::RecordNotFound, with: :render_404
+    rescue_from Exception, :with => :render_500
+    
+    def render_404
+      render file: Rails.root.join("public", "404.json"), status: 404
+    end
+    
+    def render_500(exception)
+        @exception = exception
+        render file: Rails.root.join("public", "500.json"), :status => 500
     end
     
 end
